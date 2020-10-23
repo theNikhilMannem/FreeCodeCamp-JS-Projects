55b853018a439ca1dabb84974bc7af53573c8d80
Nikhil M Kumar <38357711+theNikhilMannem@users.noreply.github.com>
2020-10-23 14:50:31 +0530

    Update palindrome-checker-simplified.js

--- palindrome-checker-simplified.js
+++ palindrome-checker-simplified.js
@@ -1,5 +1,8 @@
 /*
 A simplified and less stack occupying version of the 'palindrome-checker.js'
+This version doesn't use an inner function to return a flag value to check it with the original function!
+Yet, it's always good to defined inner functions for some complex functions where we would need some sort of flag values to check for in the procedure of the program.
+In case you ain't aware of it already, "flag" values are those that store a value (any data type, including boolean) and is used in a further step in the execution of a program to evaluate and/or compare to some other value or variable to return a required outcome from the context it's used in!
 */
 
 function checkPalindrome(string) {
