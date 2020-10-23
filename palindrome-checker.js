129d12ab6fe5d2ac93f955175a78b7a197a207c7
Nikhil M Kumar <38357711+theNikhilMannem@users.noreply.github.com>
2020-10-23 18:36:23 +0530

    Update palindrome-checker.js

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,5 +1,5 @@
-/* Problem Statement:
-
+/*
+Problem Statement ::
 Return true if the given string is a palindrome. Otherwise, return false.
 
 A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
