6ad08438e0ed195215ee5e45ad3c4058e6fcbd83
Nikhil M Kumar <38357711+theNikhilMannem@users.noreply.github.com>
2020-10-24 14:21:59 +0530

    Update caesers-cipher.js

--- caesers-cipher.js
+++ caesers-cipher.js
@@ -1,4 +1,12 @@
 /*
+Problem Statement ::
+One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
 */
 
 function rot13(str) {
@@ -32,7 +40,8 @@ function rot13(str) {
         decodedStr.push(String.fromCharCode(decodedASCIIValue));
       }
     }
-    // If the characters are not in the ASCII UpperCase limit, they are considered 'else' case: they could be punctuation and spaces (since the problem test cases are limited to UpperCase Alphabets, we don't care about handling LowerCase Alphabets that start from 97 (for 'a') and end at 122 (for 'z)).
+    // If the characters are not in the ASCII UpperCase limit, they are considered 'else' case: they could be punctuation and spaces 
+    // (since the problem test cases are limited to UpperCase Alphabets, we don't care about handling LowerCase Alphabets that start from 97 (for 'a') and end at 122 (for 'z)).
     else {
       decodedStr.push(str[index]);
     }
